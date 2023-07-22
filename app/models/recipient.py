7e5d0929bee1539fc1a6e02ed4003678d7f4aee1
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-22 16:15:04 -0300

    Testa inclusão de nova coluna via migração
    Fix #5

--- app/models/recipient.py
+++ app/models/recipient.py
@@ -7,9 +7,10 @@ from . import (
 class Recipient(db.Model, Model):
     name = db.Column(db.String(255), nullable=False)
     birthdate = db.Column(db.String(255))
+    list_kind = db.Column(db.String(255))
 
     def __repr__(self):
-        return f"Recipient(id: '{self.id}', name: '{self.name}')"
+        return f"Recipient(id: '{self.id}', name: '{self.name}', lista:'{self.list_kind}')"
 
 
 class RecipientSchema(ModelSchema):
